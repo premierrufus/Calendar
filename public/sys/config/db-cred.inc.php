682e268c8e42f2d194c8cba6329b7d80d7b16103
premierrufus <banded.gabriel@gmail.com>
2017-11-22 17:38:18 -0500

    moved file

--- public/sys/config/db-cred.inc.php
+++ public/sys/config/db-cred.inc.php
@@ -1,30 +1,3 @@
 <?php
 
-declare(strict_types=1);
-
-/*
- * Create an empty  array to store constants
- */
-$C = array();
-
-/*
- * The database host URL
- */
-$C['DB_HOST'] = 'localhost';
-
-/*
- * The database username
- */
-$C['DB_USER'] = 'root';
-
-/*
- * The database password
- */
-$C['DB_PASS'] = 'a_strong_password';
-
-/*
- * The name of the databse to work with
- */
-$C['DB_NAME'] = 'calendar';
-
 ?>
