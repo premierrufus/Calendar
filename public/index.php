0223b1370c2e55e9d25f4d47a75f0a93dbeea629
premierrufus <banded.gabriel@gmail.com>
2017-11-22 14:48:49 -0500

    moved echo again

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ echo "<h1>Hello!</h1>";
  * Load the calendar for January
  */
 $cal = new Calendar($dbo, "2018-01-01 12:00:00");
-
+echo "<h2>Hello!</h2>";
 if ( is_object ($cal) )
 {
 	echo "<pre>", var_dump($cal), "</pre>";
