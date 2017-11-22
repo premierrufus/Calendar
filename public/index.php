118a329eb28ca1ab5855f1a10623e117441c8746
premierrufus <banded.gabriel@gmail.com>
2017-11-22 13:01:32 -0500

    echo test

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ include_once '../sys/core/init.inc.php';
  * Load the calendar for January
  */
 $cal = new Calendar($dbo, "2018-01-01 12:00:00");
+echo "<h1>Hello!</h1>";
 
 if ( is_object ($cal) )
 {
